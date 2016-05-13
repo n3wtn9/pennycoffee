53827fc7b128ce7617352c23bf7f99bffda14e82
Newton Truong <newton@qurius-inc.com>
2016-05-12 22:19:19 -0700

    added routes to pages

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+
+import { Welcome } from './pages/welcome'
+import { About } from './pages/about'
 
 export class App extends Component {
 
   render(){
     return (
-      <div>Hello World</div>
+      <Router history={browserHistory}>
+        <Route path="/">
+          <IndexRoute component={Welcome}/> 
+          <Route path="about" component={About}/>
+        </Route>
+      </Router>
     )
   }
 }
